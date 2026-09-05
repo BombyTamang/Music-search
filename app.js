b3efefc6b238d7e3a9b05c153b0b1e917f008a49
BombyTamang <bhomtamang92@gmail.com>
2026-09-05 16:55:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,8 @@
 const input = document.querySelector("#searchInput");
 const btn = document.querySelector("#searchBtn");
 const song_card = document.querySelector(".song-card");
-
 const search_Result = document.querySelector("#results");
-
-/* const songTitle = song_card.querySelector("h3");
-const songArtist = song_card.querySelector("p");
-const coverImg = song_card.querySelector("img"); */
-
+const search_result_count = document.querySelector("h2");
 
 
 const base_URL= "https://api.audius.co/v1/tracks/search";
@@ -21,29 +16,10 @@ const getDetails = async ()=>{
     const response = await fetch(new_URL);
     const result = await response.json();
 
-
- //   const title = result.data[0].title;
- //   const artist = result.data[0].user.name;
-   // const cover = result.data[0].artwork["480x480"];
-
- //   songTitle.innerText = title;
- //   songArtist.innerText = artist;
- //   coverImg.src=cover;
-
     result.data.forEach((track)=>{
         console.log(track.title);
     });
 
-
-/*     search_Result.innerHTML = 
-        `<div class="song-card" alt="Album cover">
-            <img src="${result.data[0].artwork["480x480"]}">
-                <div class="song-info">
-                    <h3>${result.data[0].title}</h3>
-                    <p>${result.data[0].user.name}</p>
-                    <button>▶ Play</button>
-                </div>
-        </div>`; */
     search_Result.innerHTML = "";
      for(let i=0 ; i<result.data.length ;i++){
         search_Result.innerHTML += 
@@ -55,11 +31,12 @@ const getDetails = async ()=>{
                         <audio controls src="${result.data[i].stream.url}"></audio>
                         
                     </div>
-            </div>`;     
+            </div>`;  
+             
      }
 
      const players = document.querySelectorAll("audio");
-     
+     search_result_count.innerText=`Search Results ${result.data.length}`;
 
      players.forEach((player)=>{
         player.addEventListener("play", ()=>{
@@ -74,13 +51,6 @@ const getDetails = async ()=>{
             console.log("This song could not be played", player.src);
         })
      })
-
-
- // <button>▶ Play</button>
-
-
-
-
 }
 
 
